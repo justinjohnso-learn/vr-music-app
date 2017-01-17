973fb41d59368a5f348af278cfb1d794556d411c
Justin Johnson <justinaugustejohnson@gmail.com>
2017-01-17 13:57:55 -0500

    mobile audio works on controller button click!

--- workspace/aframe-boilerplate/components/main.js
+++ workspace/aframe-boilerplate/components/main.js
@@ -1,23 +1,29 @@
-$(document).ready(function() {
+var soundID = "castle";
+function loadSound () {
+  createjs.Sound.registerSound("castle.mp3", soundID);
+}
 
-  console.log( "ready!"  );
+function playSound () {
+  createjs.Sound.play(soundID);
+}
 
+function setEventListeners () {
   const scene = document.querySelector('a-scene')
   scene.addEventListener('proxycontrols.paircode', function (e) {
     console.log(e.detail.pairCode);
   });
-
   const camera = $('#camera')
+  $(camera).on('gamepadbuttondown', function(){
+    playSound()
+    console.log("BANANA")
+  })
   //camera.addEventListener('gamepadbuttondown', function (e) {
     //console.log('Button "%d" has been pressed.', e.index);
   //});
-  
-  $(camera).on('gamepadbuttondown', function(e){
-    console.log(e.index)
-    console.log("I hate cucumbers")
-  })
+  console.log("event listeners set!")
+}
 
+$(document).ready(function() {
+  loadSound();
+  setEventListeners();
 });
-
-
-
