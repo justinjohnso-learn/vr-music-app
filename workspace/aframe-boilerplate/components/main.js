da3bf5d2d5ecfc70275b65581f65de1c7ca894ca
Justin Johnson <justinaugustejohnson@gmail.com>
2017-01-17 17:27:10 -0500

    aaaand individual button presses work!

--- workspace/aframe-boilerplate/components/main.js
+++ workspace/aframe-boilerplate/components/main.js
@@ -1,10 +1,35 @@
-var soundID = "castle";
+let soundID = "castle";
+
 function loadSound () {
   createjs.Sound.registerSound("castle.mp3", soundID);
+  console.log("sound loaded!")
 }
 
 function playSound () {
   createjs.Sound.play(soundID);
+}
+
+function gampadListeners () {
+  const gamepadX = $('#camera').on('gamepadbuttondown:0', function () {
+    cursor.dataset.button = "A"
+    cursor.components.cursor.onMouseDown();
+    cursor.components.cursor.onMouseUp();
+    //console.log("A")
+  });
+
+  const gamepadRT = $('#camera').on('gamepadbuttondown:7', function () {
+    cursor.dataset.button = "RT"
+    cursor.components.cursor.onMouseDown();
+    cursor.components.cursor.onMouseUp();
+    //console.log("RT")
+  });
+
+  const gamepadB = $('#camera').on('gamepadbuttondown:1', function () {
+    cursor.dataset.button = "B"
+    cursor.components.cursor.onMouseDown();
+    cursor.components.cursor.onMouseUp();
+    //console.log("B")
+  });
 }
 
 function setEventListeners () {
@@ -14,12 +39,45 @@ function setEventListeners () {
   });
   const camera = $('#camera')
   $(camera).on('gamepadbuttondown', function(){
-    playSound()
-    console.log("BANANA")
+    //playSound()
+    //console.log("BANANA")
   })
+
+  // Component to change to random color on click.
+  AFRAME.registerComponent('cursor-listener', {
+    init: function () {
+      //let COLORS = ['red', 'green', 'blue'];
+      let buttonColor = "purple"
+      $(this.el).on('click', function (evt) {
+        let cursorData  = evt.originalEvent.detail.cursorEl.dataset.button
+        if (cursorData === "A") {
+          buttonColor = "blue"
+        } else if (cursorData === "B") {
+          buttonColor = "green"
+        } else if (cursorData === "RT") {
+          buttonColor = "red"
+        }
+        //let randomIndex = Math.floor(Math.random() * COLORS.length);
+        this.setAttribute('material', 'color', buttonColor);
+        //console.log(clicker)
+      });
+    }
+  });
+
+  //AFRAME.registerComponent('#box', {
+    //init: function () {
+      //this.el.addEventListener('click', function(){
+        //console.log("toast")
+      //})
+    //}
+  //})
+  
   //camera.addEventListener('gamepadbuttondown', function (e) {
     //console.log('Button "%d" has been pressed.', e.index);
   //});
+  
+  gampadListeners();
+
   console.log("event listeners set!")
 }
 
