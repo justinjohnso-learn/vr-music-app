0312fa0341793c99b13b030c6de3c66fe82c49ec
Justin Johnson <justinaugustejohnson@gmail.com>
2017-01-19 11:24:41 -0500

    better soundjs implementation

--- workspace/aframe-boilerplate/components/main.js
+++ workspace/aframe-boilerplate/components/main.js
@@ -1,21 +1,21 @@
 // Handle audio
 // DON'T FORGET TO TAP ON MOBILE SCREEN TO UNLOCK AUDIO
 //
-function loadSound () {
-  createjs.Sound.registerSound("castle.mp3", "castle");
-  createjs.Sound.registerSound("spine.mp3", "spine");
-  createjs.Sound.registerSound("tomes.mp3", "tomes");
-  createjs.Sound.addEventListener("fileload", handleFileLoad);
-  function handleFileLoad(event) {
-    var scene = document.querySelector('a-scene')
-    var audioEntity = document.createElement('a-entity')
-    audioEntity.id = event.id;
-    scene.appendChild(audioEntity);
-    console.log(audioEntity)
-    console.log("Preloaded:", event.src);
-    //audioEntity.setAttribute('fireAudio', createjs.Sound.play(event.id))
-  }
-}
+//function loadSound () {
+  //createjs.Sound.registerSound("castle.mp3", "castle");
+  //createjs.Sound.registerSound("spine.mp3", "spine");
+  //createjs.Sound.registerSound("tomes.mp3", "tomes");
+  //createjs.Sound.addEventListener("fileload", handleFileLoad);
+  //function handleFileLoad(event) {
+    //var scene = document.querySelector('a-scene')
+    //var audioEntity = document.createElement('a-entity')
+    //audioEntity.id = event.id;
+    //scene.appendChild(audioEntity);
+    //console.log(audioEntity)
+    //console.log("Preloaded:", event.src);
+    ////audioEntity.setAttribute('fireAudio', createjs.Sound.play(event.id))
+  //}
+//}
 
 //var soundID = "";
 //function playSound (soundID) {
@@ -35,30 +35,30 @@ function loadSound () {
 function gamepadListeners () {
   $('#camera').on('gamepadbuttondown:0', function () {
     var intersectedEl = cursor.components.cursor.intersectedEl
-    var audio = intersectedEl.components['audio']
+    var soundjs = intersectedEl.components['soundjs']
     cursor.emit('gamepadA', { cursorTarget: intersectedEl,
                               buttonColor: 'green',
-                              audio: audio
+                              soundjs: soundjs
                               }, true)
     console.log("A")
   });
 
   $('#camera').on('gamepadbuttondown:7', function () {
     var intersectedEl = cursor.components.cursor.intersectedEl
-    var audio = intersectedEl.components['audio']
+    var soundjs = intersectedEl.components['soundjs']
     cursor.emit('gamepadRT', { cursorTarget: intersectedEl,
                               buttonColor: 'red',
-                              audio: audio
+                              soundjs: soundjs
                               }, true)
     console.log("RT")
   });
 
   $('#camera').on('gamepadbuttondown:1', function () {
     var intersectedEl = cursor.components.cursor.intersectedEl
-    var audio = intersectedEl.components['audio']
+    var soundjs = intersectedEl.components['soundjs']
     cursor.emit('gamepadB', { cursorTarget: intersectedEl,
                               buttonColor: 'blue',
-                              audio: audio
+                              soundjs: soundjs
                               }, true)
     console.log("B")
   });
@@ -75,8 +75,8 @@ function setEventListeners () {
   scene.addEventListener('gamepadA', function(e){
     var cursorTarget = e.detail.cursorTarget
     var buttonColor = e.detail.buttonColor
-    var audio = e.detail.audio.data
-    playSound(audio)
+    var soundjs = e.detail.soundjs
+    soundjs.soundjsPlay()
     cursorTarget.setAttribute('material', 'color', buttonColor);
     //console.log(e, e.detail)
   })
@@ -84,8 +84,8 @@ function setEventListeners () {
   scene.addEventListener('gamepadRT', function(e){
     var cursorTarget = e.detail.cursorTarget
     var buttonColor = e.detail.buttonColor
-    var audio = e.detail.audio.data
-    loopSound(audio)
+    var soundjs = e.detail.soundjs
+    soundjs.soundjsLoop()
     cursorTarget.setAttribute('material', 'color', buttonColor);
     //console.log(e, e.detail)
   })
@@ -93,8 +93,8 @@ function setEventListeners () {
   scene.addEventListener('gamepadB', function(e){
     var cursorTarget = e.detail.cursorTarget
     var buttonColor = e.detail.buttonColor
-    var audio = e.detail.audio.data
-    stopSound(audio)
+    var soundjs = e.detail.soundjs
+    soundjs.soundjsStop()
     cursorTarget.setAttribute('material', 'color', buttonColor);
     //console.log(e, e.detail)
   })
@@ -105,12 +105,33 @@ function setEventListeners () {
 
 // Init entity audio component
 //
-AFRAME.registerComponent('audio', {
-  schema: 
-    {type: 'string'}
+AFRAME.registerComponent('soundjs', {
+  schema: {
+    soundjsID: {type: 'string'},
+    soundjsSrc: {type: 'string'}
+  },
+  init: function() {
+    var soundID = this.data.soundjsID;
+    var soundSrc = this.data.soundjsSrc;
+    console.log(this)
+    createjs.Sound.registerSound(soundSrc, soundID);
+    createjs.Sound.addEventListener("fileload", handleFileLoad);
+    function handleFileLoad(event) {
+      console.log("Preloaded:", event.src);
+    }
+  },
+  soundjsPlay: function(){
+    createjs.Sound.play(this.data.soundjsID)
+  },
+  soundjsStop: function(){
+    createjs.Sound.stop(this.data.soundjsID)
+  },
+  soundjsLoop: function(){
+    createjs.Sound.play(this.data.soundjsID, {loop: -1});
+  }
 });
 
 $(document).ready(function() {
-  loadSound();
+  //loadSound();
   setEventListeners();
 });
