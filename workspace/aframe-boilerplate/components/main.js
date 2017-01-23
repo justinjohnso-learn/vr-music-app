4508f90ab20defe019aec2f0eb0fd37e1b643c8b
Justin Johnson <justinaugustejohnson@gmail.com>
2017-01-23 07:39:31 -0500

    add instruments

--- workspace/aframe-boilerplate/components/main.js
+++ workspace/aframe-boilerplate/components/main.js
@@ -4,21 +4,26 @@
 // Handle gamepad click functions
 //
 function gamepadListeners () {
-  $('a-scene').on('gamepadbuttondown:0', function () {
+  $('#camera').on('gamepadbuttondown:0', function () {
     var intersectedEl = cursor.components.cursor.intersectedEl
     var soundjs = intersectedEl.components['soundjs']
     intersectedEl.emit('gamepadA', { cursorTarget: intersectedEl,
-                              buttonColor: 'green',
                               soundjs: soundjs
                               }, true)
     console.log("A")
+  });
+
+  $('#camera').on('gamepadbuttondown:6', function () {
+    //var intersectedEl = cursor.components.cursor.intersectedEl
+    //var soundjs = intersectedEl.components['soundjs']
+    document.querySelector('a-scene').emit('gamepadLT', true)
+    console.log("LT")
   });
 
   $('#camera').on('gamepadbuttondown:7', function () {
     var intersectedEl = cursor.components.cursor.intersectedEl
     var soundjs = intersectedEl.components['soundjs']
     intersectedEl.emit('gamepadRT', { cursorTarget: intersectedEl,
-                              buttonColor: 'red',
                               soundjs: soundjs
                               }, true)
     console.log("RT")
@@ -28,10 +33,18 @@ function gamepadListeners () {
     var intersectedEl = cursor.components.cursor.intersectedEl
     var soundjs = intersectedEl.components['soundjs']
     intersectedEl.emit('gamepadB', { cursorTarget: intersectedEl,
-                              buttonColor: 'blue',
                               soundjs: soundjs
                               }, true)
     console.log("B")
+  });
+  
+  $('#camera').on('gamepadbuttondown:3', function () {
+    var intersectedEl = cursor.components.cursor.intersectedEl
+    var soundjs = intersectedEl.components['soundjs']
+    intersectedEl.emit('gamepadY', { cursorTarget: intersectedEl,
+                              soundjs: soundjs
+                              }, true)
+    console.log("Y")
   });
 }
 
@@ -46,37 +59,44 @@ function setEventListeners () {
   scene.addEventListener('gamepadA', function(e){
     var cursorTarget = e.detail.cursorTarget
     var soundjs = e.detail.soundjs
-    //console.log(e)
-    soundjs.soundjsPlay()
-    //console.log (soundjs)
-    //cursorTarget.setAttribute('material', 'color', buttonColor);
-    cursorTarget.removeAttribute('animation', {property: 'color', dir: 'alternate', dur: 1000, easing: 'easeInSine', loop: true, to: '#F00'})
-    cursorTarget.setAttribute('animation', {property: 'color', dir: 'alternate', dur: 1000, easing: 'easeInSine', loop: true, to: '#0F0'})
+    soundjs.soundjsPlay(cursorTarget)
+    cursorTarget.removeAttribute('animation', {pauseEvents: 'soundDone', property: 'color', dir: 'alternate', dur: 1500, easing: 'easeInSine', loop: true, to: '#F00'})
+    cursorTarget.setAttribute('animation', {pauseEvents: 'soundDone', property: 'color', dir: 'alternate', dur: 1500, easing: 'easeInSine', loop: true, to: '#228B22'})
+  })
+
+
+  scene.addEventListener('gamepadLT', function(e){
+    soundjsStopAll()
   })
 
   scene.addEventListener('gamepadRT', function(e){
     var cursorTarget = e.detail.cursorTarget
     var soundjs = e.detail.soundjs
     soundjs.soundjsLoop()
-    //cursorTarget.setAttribute('material', 'color', buttonColor);
-    cursorTarget.removeAttribute('animation', {property: 'color', dir: 'alternate', dur: 1000, easing: 'easeInSine', loop: true, to: '#0F0'})
-    cursorTarget.setAttribute('animation', {property: 'color', dir: 'alternate', dur: 1000, easing: 'easeInSine', loop: true, to: '#F00'})
+    cursorTarget.removeAttribute('animation', {pauseEvents: 'soundDone', property: 'color', dir: 'alternate', dur: 1500, easing: 'easeInSine', loop: true, to: '#228B22'})
+    cursorTarget.setAttribute('animation', {pauseEvents: 'soundDone', property: 'color', dir: 'alternate', dur: 1500, easing: 'easeInSine', loop: true, to: '#F00'})
   })
 
+  scene.addEventListener('gamepadY', function(e){
+    var cursorTarget = e.detail.cursorTarget
+    var soundjs = e.detail.soundjs
+    soundjs.soundjsPause()
+    cursorTarget.removeAttribute('animation', {pauseEvents: 'soundDone', property: 'color', dir: 'alternate', dur: 1500, easing: 'easeInSine', loop: true, to: '#228B22'})
+    cursorTarget.removeAttribute('animation', {pauseEvents: 'soundDone', property: 'color', dir: 'alternate', dur: 1500, easing: 'easeInSine', loop: true, to: '#F00'})
+    //cursorTarget.setAttribute('material', 'color', '#6060FF');
+  })
+  
   scene.addEventListener('gamepadB', function(e){
     var cursorTarget = e.detail.cursorTarget
-    //var targetColor = cursorTarget.components.material.data.color
     var soundjs = e.detail.soundjs
     soundjs.soundjsStop()
-    cursorTarget.removeAttribute('animation', {property: 'color', dir: 'alternate', dur: 1000, easing: 'easeInSine', loop: true, to: '#0F0'})
-    cursorTarget.removeAttribute('animation', {property: 'color', dir: 'alternate', dur: 1000, easing: 'easeInSine', loop: true, to: '#F00'})
+    cursorTarget.removeAttribute('animation', {pauseEvents: 'soundDone', property: 'color', dir: 'alternate', dur: 1500, easing: 'easeInSine', loop: true, to: '#228B22'})
+    cursorTarget.removeAttribute('animation', {pauseEvents: 'soundDone', property: 'color', dir: 'alternate', dur: 1500, easing: 'easeInSine', loop: true, to: '#F00'})
     cursorTarget.setAttribute('material', 'color', '#6060FF');
-    //console.log(targetColor)
-    //cursorTarget.setAttribute('animation', {property: 'color', dur: 0, to: targetColor})
   })
 
   function handleFileLoad(event) {
-    console.log("Preloaded:", event.src);
+    console.log("Preloaded:", event.id);
     //console.log(event)
   }
   createjs.Sound.addEventListener("fileload", handleFileLoad);
@@ -86,6 +106,12 @@ function setEventListeners () {
   console.log("event listeners set!")
 }
 
+function soundjsStopAll(){
+  createjs.Sound.stop()
+  document.querySelectorAll('[soundjs]').forEach(function(object){
+    object.emit("soundDone")
+  })
+}
 
 // Init entity audio component
 //
@@ -104,21 +130,44 @@ AFRAME.registerComponent('soundjs', {
     var myInstance = createjs.Sound.createInstance(soundID)
     myInstance.on("complete", function(){
       el.emit("soundDone")
+      console.log("complete")
     })
     this.data.soundjsInstance = myInstance
     //el.setAttribute('audioanalyser', {src: myInstance});
   },
 
-  soundjsPlay: function(){
+  soundjsPlay: function(el){
     var myInstance = this.data.soundjsInstance
+    myInstance.loop = 0
+    myInstance.on("complete", function(){
+      el.emit("soundDone")
+      console.log("complete")
+    })
     myInstance.play(this.data.id, {interrupt: createjs.Sound.INTERRUPT_ANY})
+    this.data.soundjsInstance = myInstance
+    if (this.el.localName !== 'a-cylinder'){
+      myInstance.duration = 7100
+    };
+    if (this.el.id === "note"){
+      myInstance.duration = 444
+    };
   },
   soundjsStop: function(){
     this.data.soundjsInstance.stop()
   },
+  soundjsPause: function(){
+    var myInstance = this.data.soundjsInstance
+    myInstance.paused = true
+  },
   soundjsLoop: function(){
     this.data.soundjsInstance.stop()
     var myInstance = createjs.Sound.play(this.data.id, {loop: -1})
+    if (this.el.localName !== 'a-cylinder'){
+      myInstance.duration = 7100
+    };
+    if (this.el.id === "note"){
+      myInstance.duration = 444
+    };
     this.data.soundjsInstance = myInstance
   }
 });
